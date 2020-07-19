813945caf2e8fd50e0b712ee4adf9a5e2c836bc0
sprojects18 <info@sgenschow.de>
2020-07-19 09:10:23 +0000

    checks comleted

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,33 @@
-def arithmetic_arranger(problems):
-  # Test2
+import re
 
-    return arranged_problems
+def arithmetic_arranger(problems, flag=True):
+  # transform input data --------------
+  # list of list with 3 elements in each sublist
+  data = list() 
+  for problem in problems:
+    t = problem.split(' ')
+    if(len(t)!=3):
+      return "Wrong input format. Should be e.g.: 23 + 44."
+    data.append(t)
+
+
+  # checks ---------------------------
+  if len(problems)>5:
+    return "Error: Too many problems."
+  
+  for d in data:
+    if d[1]!='+' and d[1]!='-':
+      return "Error: Operator must be '+' or '-'."
+    if re.search("[^0-9]+", d[0]) or re.search("[^0-9]+", d[2]):
+      return "Error: Numbers must only contain digits."   
+    if len(d[0])>4 or len(d[2])>4:
+      return "Error: Numbers cannot be more than four digits."
+
+  # main ----------------------------
+  
+
+
+
+  arranged_problems = None
+  return arranged_problems
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # This entrypoint file to be used in development. Start by reading README.md
 from arithmetic_arranger import arithmetic_arranger
 from unittest import main
+import replit
 
+replit.clear() # clear output in console
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
